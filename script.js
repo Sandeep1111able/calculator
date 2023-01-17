3b1a6f71f76c6e0cd287fa78c36ed47953abc5b8
Sandeep Subedi <sandeep1111able@gmail.com>
2023-01-17 19:50:28 +0545

    Perform maths function for two numbers only

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const numbers = document.querySelectorAll(".numbers");
+const screen = document.querySelector(".screen");
+const operators = document.querySelectorAll(".operators");
+let firstNumber = "";
+let secondNumber = "";
+let choosenOperator = "";
+let finalValue = "";
+
 const add = function (a, b) {
   return a + b;
 };
@@ -14,4 +22,41 @@ const divide = function (a, b) {
   return a / b;
 };
 
-const operate = function (operator, firstNumber, secondNumber) {};
+const operate = function (operator, firstNumber, secondNumber) {
+  switch (operator) {
+    case "+":
+      return add(firstNumber, secondNumber);
+    case "-":
+      return subtract(firstNumber, secondNumber);
+    case "x":
+      return multiply(firstNumber, secondNumber);
+    case "/":
+      return divide(firstNumber, secondNumber);
+  }
+};
+
+const populateDisplay = function (e) {
+  if (choosenOperator === "") {
+    screen.textContent += this.textContent;
+    firstNumber = Number(screen.textContent);
+  } else {
+    screen.textContent += this.textContent;
+    secondNumber = Number(screen.textContent);
+  }
+};
+
+numbers.forEach((number) => {
+  number.addEventListener("click", populateDisplay);
+});
+
+operators.forEach((operator) => {
+  operator.addEventListener("click", (e) => {
+    if (operator.textContent !== "=") {
+      screen.textContent = "";
+      choosenOperator = operator.textContent;
+    } else {
+      finalValue = operate(choosenOperator, firstNumber, secondNumber);
+      screen.textContent = finalValue;
+    }
+  });
+});
